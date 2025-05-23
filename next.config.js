0f3e57544f4d0a877e3fa534975e6346761d6ce6
Ruben Berreguero <rubenbr85@gmail.com>
2025-05-23 23:29:15 +0200

    UPDATED: Configurar encabezados CORS en next.config.js para permitir solicitudes desde cualquier origen y definir métodos y encabezados permitidos.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
     API_KEY: process.env.API_KEY,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
   },
-}
+  async headers() {
+    return [
+      {
+        // Aplica estas reglas a todas las rutas de la API
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Permite cualquier origen
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS', // Métodos permitidos
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization', // Encabezados permitidos
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
