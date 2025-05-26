21c4df4dfd269e1c8f682661b31af917eb2ce601
Ruben Berreguero <rubenbr85@gmail.com>
2025-05-27 00:22:37 +0200

    UPDATED: selector de fuente alfabeto

--- src/services/characters/CharacterService.ts
+++ src/services/characters/CharacterService.ts
@@ -1,9 +1,10 @@
 import { Character } from '../../models/character.interface';
 import { CharacterSource } from '../../models/character-source.interface';
-import { SOURCE_POKEMON_ID, SOURCE_DISNEY_ID, SOURCE_PATRULLA_ID } from '../../constants/sources';
+import { SOURCE_POKEMON_ID, SOURCE_DISNEY_ID, SOURCE_PATRULLA_ID, SOURCE_ALPHABET_ID } from '../../constants/sources';
 import { PokemonService } from './PokemonService';
 import { DisneyService } from './DisneyService';
 import { PatrullaService } from './PatrullaService';
+import { AlphabetService } from './AlphabetService';
 import { DifficultyService } from '../difficulty/difficulty.service';
 
 export class CharacterService {
@@ -20,6 +21,9 @@ export class CharacterService {
           break;
         case SOURCE_PATRULLA_ID:
           characters = PatrullaService.getCharacters();
+          break;
+        case SOURCE_ALPHABET_ID:
+          characters = AlphabetService.getCharacters();
           break;
         default:
           throw new Error(`Fuente desconocida: ${source.nombre}`);
